f2e9033369d768a7ba765ef73434fa70eff5bed3
ArbIsmajli <ismajliarb@gmail.com>
2024-12-03 18:40:39 +0100

    loops vazhdim

--- loops.js
+++ loops.js
@@ -2,6 +2,27 @@
 //     console.log(a);
 // }
 
-for (let r =0; r < 100; r +=2) {
-    console.log(r);
+// for (let r =0; r < 100; r +=2) {
+//     console.log(r);
+// }
+// for(let i = 1; i < 9; i++) {
+//     console.log(i);
+// }
+for(let i = 1; i <= 8;i++){
+    console.log(i)
+}
+
+for (let i = 1; i <= 8; i++) {
+    if (i === 7) {
+        continue;
+    }
+    console.log(i);
+}
+
+let start, _stop;
+
+start = prompt("prej cilit numer po don me printu ne konzolle");
+_stop = prompt("deri te cili numer po don me printu ne konzolle");
+for(let i = start; i <= _stop; i ++) {
+    console.log(i);
 }
